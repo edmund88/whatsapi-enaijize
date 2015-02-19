d9769e706d9b0a2738fdc91a47b434c5e2243cb1
InsidiousMiss <crashd.burnd@gmail.com>
2015-02-19 18:28:24 -0500

    debug

--- src/events/Events.php
+++ src/events/Events.php
@@ -52,8 +52,8 @@ class MyEvents extends AllEvents
 //        'onGroupsChatEnd',
 //        'onGroupsParticipantsAdd',
 //        'onGroupsParticipantsRemove',
-//        'onLogin',
-//        'onLoginFailed',
+        'onLogin',
+        'onLoginFailed',
 //        'onAccountExpired',
 //        'onMediaMessageSent',
 //        'onMediaUploadFailed',
@@ -88,9 +88,54 @@ class MyEvents extends AllEvents
 
     public function onGetMessage( $mynumber, $from, $id, $type, $time, $name, $body )
     {
+		$from = chop($from,'@s.whatsapp.net');
+		$body = strtoupper($body);
+	
+		$servername = "ec2-50-19-236-178.compute-1.amazonaws.com";
+		$port = "5432";
+		$username = "dsrxtxfrujicul";
+		$password = "abU_kylvHNeWMFJRwzPnQajP8P";
+		$dbname = "db7ctk55jodcoo";
+		
+		$pg_connection_string = "dbname=" . $dbname . " host=" . $servername . " port=" . $port . " user=" . $username . " password=" . $password . " sslmode=require";
 
+		$db = pg_connect($pg_connection_string) or die("Could not connect");
+	
+		$sql = "SELECT * FROM messages WHERE sender = '" . $from . "'";
+		$result = pg_query($db, $sql);
+		$lastmessage = pg_fetch_assoc($result);
+
+		if(!$lastmessage) {
+			$sql = "INSERT INTO messages (sender, time_sent, message, new) VALUES ('" . $from . "', " . $time . ", '" . $body . "', TRUE)";
+			if (pg_query($db, $sql)) {
+				echo "New record created successfully<br/>";
+			} else {
+				echo "Error: " . $sql;
+			}
+		}
+		else {
+			$sql = "UPDATE messages SET time_sent='" . $time . "', message='" . $body . "', prev_message='" . $lastmessage['message'] . "', new=TRUE WHERE sender='" . $from . "'";		
+			if (pg_query($db, $sql)) {
+				echo "Record updated successfully<br/>";
+			} else {
+				echo "Error: " . $sql;
+			}			
+		}
+		
+		pg_close($db);
     }
 
+    public function onLogin($mynumber)
+    {
+        echo "Logged in.";
+    }
+
+    public function onLoginFailed($mynumber, $data)
+    {
+        echo "Login failed.";
+    }	
+	
+	
     public function onSendMessage($mynumber, $target, $messageId, $node)
     {
         echo "Message $messageId sent to $target.</br>";
